355d79fc58d473c408f334e6d14e0ba99fce1c9d
Leonardo De Paula <92495561+leonarddepaula@users.noreply.github.com>
2022-09-19 09:04:15 -0300

    first commit shop

---
+++ loja-computers/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NaoEncontradoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatSnackBarModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
